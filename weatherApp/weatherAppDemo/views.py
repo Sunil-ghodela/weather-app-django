fb9a76a4783af9cb79fb34aabfeb4520cf4063dd
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-09-24 15:39:18 +0530

    implement one city weather info in template

--- weatherApp/weatherAppDemo/views.py
+++ weatherApp/weatherAppDemo/views.py
@@ -1,6 +1,26 @@
+import requests
+import json
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "index.html"
-# Create your views here.
+
+
+
+def weather(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='
+    city = 'Delhi'
+
+    r = requests.get(url.format(city)).json()
+
+    weather_data = {
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+
+    context = { 'weather_data': weather_data }
+
+    return render(request, 'weather/weather.html', context)
