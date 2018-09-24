aa1d09d5abe5e690f42a438489ab43bde946f875
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-09-24 12:52:47 +0530

    settings djagno project

---
+++ my_virtual_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/curofy/Desktop/name/project/py/django-1st-week/weather-app-django/my_virtual_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ weatherApp/weatherAppDemo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeatherappdemoConfig(AppConfig):
+    name = 'weatherAppDemo'
